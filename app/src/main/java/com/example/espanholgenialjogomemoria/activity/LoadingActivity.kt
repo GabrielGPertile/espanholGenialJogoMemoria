fd485e1d569628e611f709fe3245522adfa5d401
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 14:40:45 -0300

    feat: adicionando a tela de carregamento, que ao final de 5 segundos entra na tela de login

---
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/LoadingActivity.kt
@@ -0,0 +1,32 @@
+package com.example.espanholgenialjogomemoria.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.espanholgenialjogomemoria.R
+import com.example.espanholgenialjogomemoria.viewholder.ImageViewHolder
+
+class LoadingActivity : AppCompatActivity()
+{
+    private lateinit var imageViewHolder: ImageViewHolder
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_activity)
+
+        imageViewHolder = ImageViewHolder(this)
+
+        // Definir um atraso de 3 segundos (3000 milissegundos)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            // Fechar a LoadingActivity para que o usuário não consiga voltar para ela
+            finish()
+        },3000)
+
+    }
+}

a6d29f5b8f04e881d916a098547a5145fe3cdd4a
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 19:44:24 -0300

    feat: inicializando as variavéis e fazendo a configuração dos botões

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/ForgetPasswordReset.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/ForgetPasswordReset.kt
@@ -1,8 +1,11 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.viewholder.ForgetPasswordResetViewHolder
 import com.example.espanholgenialjogomemoria.viewholder.ImageViewHolder
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgetPasswordReset: AppCompatActivity()
@@ -11,4 +14,27 @@ class ForgetPasswordReset: AppCompatActivity()
     private lateinit var imageViewHolder: ImageViewHolder
     private lateinit var forgetPasswordResetViewHolder: ForgetPasswordResetViewHolder
     private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forget_password_reset)
+
+        //Inicializa os objetos
+        imageViewHolder = ImageViewHolder(this)
+        forgetPasswordResetViewHolder = ForgetPasswordResetViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+
+        // Configuração dos Botões
+        forgetPasswordResetViewHolder.btnEnviar.setOnClickListener {
+            sendPasswordResetEmail()
+        }
+
+        forgetPasswordResetViewHolder.btnVoltar.setOnClickListener {
+            navigateBackToLogin()
+        }
+    }
 }

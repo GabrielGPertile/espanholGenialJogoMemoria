fb4ef3c7f745646fe6f52f31d3036177477ab71c
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 15:23:19 -0300

    feat: inicializando as variaveis

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/LoginActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.viewholder.ImageViewHolder
 import com.example.espanholgenialjogomemoria.viewholder.LoginActivityViewHolder
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity: AppCompatActivity()
@@ -11,4 +14,19 @@ class LoginActivity: AppCompatActivity()
     private lateinit var imageViewHolder: ImageViewHolder
     private lateinit var loginActivityViewHolder: LoginActivityViewHolder
     private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        //Inicializa os objetos
+        imageViewHolder = ImageViewHolder(this)
+        loginActivityViewHolder = LoginActivityViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+
+    }
 }

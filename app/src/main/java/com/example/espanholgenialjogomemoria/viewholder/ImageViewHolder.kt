6282f2e19bbe5334297066c648b98ff5e2d99160
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 14:36:55 -0300

    feat: criando a viewholder do carregamento da tela inicial

---
+++ app/src/main/java/com/example/espanholgenialjogomemoria/viewholder/ImageViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.espanholgenialjogomemoria.viewholder
+
+import android.app.Activity
+import android.widget.ImageView
+import com.example.espanholgenialjogomemoria.R
+
+class ImageViewHolder(activity: Activity)
+{
+    val ivImageView: ImageView = activity.findViewById(R.id.ivLogo)
+}

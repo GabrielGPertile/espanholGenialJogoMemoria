5c6f87b02a24b6bb345184e201f38f498d997f96
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:14:05 -0300

    feat: inicializando as variaveis e o menu lateral

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/UserPerfileEditableActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/UserPerfileEditableActivity.kt
@@ -2,9 +2,12 @@ package com.example.espanholgenialjogomemoria.activity
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.viewholder.UserPerfileEditableViewHolder
 import com.example.espanholgenialstorageandroid.model.UserClass
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 
@@ -16,4 +19,34 @@ class UserPerfileEditableActivity : BaseDrawerActivity()
     private lateinit var auth: FirebaseAuth
     private lateinit var storage: FirebaseStorage
     private lateinit var user: UserClass
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_perfile_editable_activity)
+
+        //Inicializa os objetos
+        userPerfileEditableViewHolder = UserPerfileEditableViewHolder(this)
+
+        //inicializando a classe user
+        user = UserClass()
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        //configura o menu lateral
+        setupDrawer(
+            userPerfileEditableViewHolder.drawerLayout,
+            userPerfileEditableViewHolder.navView,
+            userPerfileEditableViewHolder.toolbar
+        )
+
+        userPerfileEditableViewHolder.etNomeCompletoDado.clearFocus()
+        userPerfileEditableViewHolder.etIdadeDado.clearFocus()
+
+        loadProfilePhotoInDrawer()
+        loadProfilePhotoWithStrategy()
+    }
 }

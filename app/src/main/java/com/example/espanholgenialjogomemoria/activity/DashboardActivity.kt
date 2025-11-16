2d01b22b3f0923c74f32caa17c98dc9fa0e0f8e9
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:40:21 -0300

    feat: inia a variavel principal e o menu lateral

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/DashboardActivity.kt
@@ -1,8 +1,27 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.os.Bundle
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.viewholder.DashboardActivityViewHolder
 
 class DashboardActivity: BaseDrawerActivity()
 {
     private lateinit var dashboardActivityViewHolder: DashboardActivityViewHolder
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_activity)
+
+        dashboardActivityViewHolder = DashboardActivityViewHolder(this)
+
+        // 🔹 Inicializa as views do Drawer e Toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val drawerLayout =
+            findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawer_layout)
+        val navView =
+            findViewById<com.google.android.material.navigation.NavigationView>(R.id.nav_view)
+
+        setupDrawer(drawerLayout, navView, toolbar)
+        loadProfilePhotoInDrawer()
+    }
 }

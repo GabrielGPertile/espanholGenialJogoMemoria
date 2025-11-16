1b17b4de754bee2a90903be31e3c07b82a4be95c
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 21:26:24 -0300

    chore: adicionando o glide ao projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,8 @@ dependencies {
 
     // (Opcional) Login com Google
     implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }

5e60c945cd92009920e062256a2013212292335f
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:39:13 -0300

    feat: declarando a variavel principal

---
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/DashboardActivity.kt
@@ -0,0 +1,8 @@
+package com.example.espanholgenialjogomemoria.activity
+
+import com.example.espanholgenialjogomemoria.viewholder.DashboardActivityViewHolder
+
+class DashboardActivity: BaseDrawerActivity()
+{
+    private lateinit var dashboardActivityViewHolder: DashboardActivityViewHolder
+}

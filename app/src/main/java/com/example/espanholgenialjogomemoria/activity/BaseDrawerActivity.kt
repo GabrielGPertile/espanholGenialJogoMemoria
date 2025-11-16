2abed46f7241abfc3f51a2310480f05af331ebb9
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-15 21:32:11 -0300

    feat: inicializando as variaveis

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/BaseDrawerActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/BaseDrawerActivity.kt
@@ -1,8 +1,10 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.espanholgenialjogomemoria.R
 import com.google.android.material.navigation.NavigationView
 
 class BaseDrawerActivity : AppCompatActivity()
@@ -10,4 +12,25 @@ class BaseDrawerActivity : AppCompatActivity()
     protected lateinit var drawerLayout: DrawerLayout
     protected lateinit var navView: NavigationView
     protected lateinit var toggle: ActionBarDrawerToggle
+
+    protected fun setupDrawer(drawerLayout: DrawerLayout, navView: NavigationView, toolbar: androidx.appcompat.widget.Toolbar)
+    {
+        this.drawerLayout = drawerLayout
+        this.navView = navView
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        toggle.drawerArrowDrawable.color = Color.WHITE  // substitua RED pela cor que quiser
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        setupNavigationMenu()
+    }
 }

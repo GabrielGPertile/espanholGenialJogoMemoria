f910a39d5738cdba5b75579eea32a9a50c67c79f
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:29:49 -0300

    feat: adicionando a modelagem do dado do usuário

---
+++ app/src/main/java/com/example/espanholgenialjogomemoria/model/UserClass.kt
@@ -0,0 +1,9 @@
+package com.example.espanholgenialstorageandroid.model
+
+data class UserClass (
+    var uid: String = "",
+    var nomeCompleto: String = "",
+    var fotoPerfil: String = "",
+    var idade: Int = 0,
+    var email: String = "",
+)

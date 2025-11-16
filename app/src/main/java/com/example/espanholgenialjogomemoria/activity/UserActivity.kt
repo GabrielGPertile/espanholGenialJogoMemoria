beb53ce9961a9b28effc40b6f4bda63348d06c2c
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:01:31 -0300

    feat: inicializando as variaveis, o menu lateral e declarando os prototipos da funções

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/UserActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/UserActivity.kt
@@ -1,6 +1,9 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.os.Bundle
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.viewholder.UserActivityViewHolder
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -9,4 +12,32 @@ class UserActivity : BaseDrawerActivity()
     private lateinit var userActivityViewHolder: UserActivityViewHolder
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_perfile_activity)
+
+        userActivityViewHolder = UserActivityViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        setupDrawer(
+            userActivityViewHolder.drawerLayout,
+            userActivityViewHolder.navView,
+            userActivityViewHolder.toolbar
+        )
+
+        loadProfilePhotoInDrawer()
+        loadProfilePhotoWithStrategy()
+        loadUserdata()
+
+        //configuração do botão
+        userActivityViewHolder.btnEditar.setOnClickListener {
+            navigateToUserPerfileEditableActivity()
+        }
+    }
 }

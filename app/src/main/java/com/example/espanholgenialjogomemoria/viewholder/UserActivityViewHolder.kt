b68182bf416467e703cfaed01e9d3bf73110a728
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-16 14:09:20 -0300

    feat: adicionando o viewholder da tela de exibição das informações do usuário

---
+++ app/src/main/java/com/example/espanholgenialjogomemoria/viewholder/UserActivityViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.espanholgenialjogomemoria.viewholder
+
+import android.app.Activity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.espanholgenialjogomemoria.R
+import com.google.android.material.navigation.NavigationView
+
+class UserActivityViewHolder(activity: Activity)
+{
+    //layout principal
+    val ivPerfilUsuario: ImageView = activity.findViewById(R.id.ivPerfilUsuario)
+    val tvNomeCompleto: TextView = activity.findViewById(R.id.tvNomeCompleto)
+    val tvNomeCompletoDado: TextView = activity.findViewById(R.id.tvNomeCompletoDado)
+    val tvEmail: TextView = activity.findViewById(R.id.tvEmail)
+    val tvEmailDado: TextView = activity.findViewById(R.id.tvEmailDado)
+    val tvIdade: TextView = activity.findViewById(R.id.tvIdade)
+    val tvIdadeDado: TextView = activity.findViewById(R.id.tvIdadeDado)
+    val btnEditar: Button = activity.findViewById(R.id.btnEditar)
+
+    // Elementos do menu lateral
+    var toolbar: Toolbar =  activity.findViewById(R.id.toolbar)
+    val drawerLayout: DrawerLayout = activity.findViewById(R.id.drawer_layout)
+    val navView: NavigationView = activity.findViewById(R.id.nav_view)
+}

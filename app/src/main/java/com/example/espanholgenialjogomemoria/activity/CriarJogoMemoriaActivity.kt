030c31e796cb02f7c1018b70d33c6ea4cac18fb4
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-20 11:35:21 -0300

    feat: inicializando as principais variaiveis

--- app/src/main/java/com/example/espanholgenialjogomemoria/activity/CriarJogoMemoriaActivity.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/activity/CriarJogoMemoriaActivity.kt
@@ -1,8 +1,12 @@
 package com.example.espanholgenialjogomemoria.activity
 
+import android.os.Bundle
+import com.example.espanholgenialjogomemoria.R
 import com.example.espanholgenialjogomemoria.model.JogoMemoria
 import com.example.espanholgenialjogomemoria.strategy.Categoria
 import com.example.espanholgenialjogomemoria.strategy.TipoJogoMemoria
+import com.example.espanholgenialjogomemoria.viewholder.CriarJogoMemoriaViewHolder
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 
@@ -13,4 +17,27 @@ class CriarJogoMemoriaActivity: BaseDrawerActivity()
     private lateinit var jogoMemoria: JogoMemoria
     private lateinit var categoria: Categoria
     private lateinit var tipoJogoMemoria: TipoJogoMemoria
+    private lateinit var criarJogoMemoriaViewHolder: CriarJogoMemoriaViewHolder
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.criar_jogo_memoria)
+
+        criarJogoMemoriaViewHolder = CriarJogoMemoriaViewHolder(this)
+
+        //Inicializa o Auth do Firebase
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        //configura o menu lateral
+        setupDrawer(
+            criarJogoMemoriaViewHolder.drawerLayout,
+            criarJogoMemoriaViewHolder.navView,
+            criarJogoMemoriaViewHolder.toolbar
+        )
+
+        loadProfilePhotoInDrawer()
+    }
 }

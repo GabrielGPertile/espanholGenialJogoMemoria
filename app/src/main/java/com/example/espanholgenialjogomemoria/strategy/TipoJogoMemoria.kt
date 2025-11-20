d0fee7b5c8cb372846502e38800343f11c4b43fe
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-20 13:27:51 -0300

    feat: adicionando a mais um dado

--- app/src/main/java/com/example/espanholgenialjogomemoria/strategy/TipoJogoMemoria.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/strategy/TipoJogoMemoria.kt
@@ -5,6 +5,7 @@ class TipoJogoMemoria
     fun addTipoJogoMemoria(): List<String>
     {
         return listOf(
+            "Selecione um:",
             "Par_PT",
             "Par_EN",
             "Triplo"

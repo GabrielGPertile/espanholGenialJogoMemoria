6b302f96debc7f7ac1205f69c893d3b792771e8f
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-11-21 10:37:56 -0300

    fix: remodelando a classe jogo memoria

--- app/src/main/java/com/example/espanholgenialjogomemoria/model/JogoMemoria.kt
+++ app/src/main/java/com/example/espanholgenialjogomemoria/model/JogoMemoria.kt
@@ -4,7 +4,13 @@ data class JogoMemoria(
     val nome: String = "",
     val tipoJogoMemoria: String = "",
     val categoria: String = "",
-    val arquivos: List<String> = emptyList(),
+    val itens: List<ItemJogoMemoria> = emptyList(), // <-- aqui!
     val criadoPor: String = "",
     val id: String = ""
+)
+
+data class ItemJogoMemoria(
+    val imagemURL: String = "",   // sempre existe
+    val pt: String? = null,    // só quando o tipo exigir
+    val en: String? = null     // só quando o tipo exigir
 )
